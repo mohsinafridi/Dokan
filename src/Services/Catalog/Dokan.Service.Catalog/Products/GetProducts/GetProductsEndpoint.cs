b72a217529f2c19535dcf270beafd65d5c50c85b
Mohsin Azam <mohsin.afridi91@gmail.com>
2024-12-22 21:29:46 +0400

    Get Catalog endpoint added

---
+++ src/Services/Catalog/Dokan.Service.Catalog/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Dokan.Service.Catalog.Products.GetProducts;
+
+public record GetProductsResponse(IEnumerable<Product> products);
+
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var query = new GetProductsQuery();
+
+            var result = await sender.Send(query);
+
+            var response = result.Adapt<GetProductsResponse>();
+            
+            return Results.Ok(response);
+        }).WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithSummary("Get all products")
+        .WithDescription("Get all products in the catalog");
+    }
+}
---
+++ src/Services/Catalog/Dokan.Service.Catalog/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Dokan.Service.Catalog.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+internal class GetProductQueryHandler
+    (IDocumentSession session, ILogger<GetProductQueryHandler> logger)
+     : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+       
+        var products  =await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
